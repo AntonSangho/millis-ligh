a765f091b848477f92e0e2ff6acef082d820d72c
Sang Ho Lee <sanghoemail@gmail.com>
2023-04-03 12:20:56 +0900

    change to millis

--- milli-button-led.ino
+++ milli-button-led.ino
@@ -1,11 +1,11 @@
  const int t1_LED = 13;
-// unsigned int on_off = 0;
+ unsigned int on_off = 0;
 
 const int t2_LED = 12;
 unsigned int t_high = 0;
 
-// unsigned long t1_prev = 0;
-// const unsigned long t1_delay = 500;
+ unsigned long t1_prev = 0;
+ const unsigned long t1_delay = 500;
 
 unsigned long t2_prev = 0;
 const unsigned long t2_delay = 10;
@@ -21,14 +21,19 @@ void setup() {
 }
 
 void loop() {
-  // unsigned long t1_now = millis();
-  // if (t1_now - t1_prev >= t1_delay) {
-  //   t1_prev = t1_now;
+  buttonState = digitalRead(buttonPin);
+  unsigned long t1_now = millis();
+  if (buttonState == LOW){
+    if (t1_now - t1_prev >= t1_delay) {
+    t1_prev = t1_now;
 
-  //   on_off++;
-  //   if(on_off>1) on_off =0;
-  //   digitalWrite(t1_LED, on_off);
-  // }
+    on_off++;
+    if(on_off>1) on_off =0;
+    digitalWrite(t1_LED, on_off);
+    }
+  }
+  
+  
 
   unsigned long t2_now = millis();
   if (t2_now - t2_prev >= t2_delay) {
@@ -39,15 +44,15 @@ void loop() {
     analogWrite(t2_LED, t_high);
   }
 
-  buttonState = digitalRead(buttonPin);
+  
 
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == LOW) {
-    // turn LED on:
-    digitalWrite(t1_LED, HIGH);
-  } else {
-    // turn LED off:
-    digitalWrite(t1_LED, LOW);
-  }
+  // if (buttonState == LOW) {
+  //   // turn LED on:
+  //   digitalWrite(t1_LED, HIGH);
+  // } else {
+  //   // turn LED off:
+  //   digitalWrite(t1_LED, LOW);
+  // }
 
 }
